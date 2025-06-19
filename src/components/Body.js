340f82c1f4b6b156eae1c4d36c847fb5f8325d2e
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-19 23:42:58 +0530

    body file contains only the tag line for the app

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+
+
+const Body = () => {
+
+  return (
+    <div className="body">
+        <h2>FROM THE ROOTS TO THE HIVE — WE COULDN’T DO IT WITHOUT YOU. THANK YOU!😊</h2>
+        <h4>Where good food meets green vibes — Brewed with nature, served with love.</h4>
+    </div>
+  )
+};
+
+export default Body;
