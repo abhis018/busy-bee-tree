e5906aae2f806b4a2dd04d9be9d1cc65362756ac
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-19 23:46:42 +0530

    created only the file for such section

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+const Menu = () => {
+
+  return (
+    <div className="gallery">
+        <Link to="/">
+            <button>Back to Home</button>
+        </Link>
+    </div>
+  )
+};
+
+export default Menu;
---
+++ src/components/Private.js
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom"
+
+const Private = () => {
+
+  return (
+    <div className="gallery">
+        <Link to="/">
+            <button>Back to Home</button>
+        </Link>
+    </div>
+  )
+};
+
+export default Private;
