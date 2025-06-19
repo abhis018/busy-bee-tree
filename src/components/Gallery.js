da19e8436d4a3b7f750b80e3e66080239e89f6b4
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-19 23:44:22 +0530

    file contains some images and videos and some para

---
+++ src/components/Gallery.js
@@ -0,0 +1,85 @@
+import video1 from "../images/video1.mp4";
+import video2 from "../images/video2.mp4"
+import video3 from "../images/video3.mp4"
+import video4 from "../images/video4.mp4"
+import video5 from "../images/video5.mp4"
+import img1 from "../images/270937708_611279030087361_4519147822408125663_n.webp.jpg";
+import img2 from "../images/331600316_713419987155196_1885277905904417843_n.webp.jpg";
+import img3 from "../images/491899981_18015234899706601_7142213015893812756_n.webp";
+import img4 from "../images/501277424_18018293480706601_4403042401682716580_n.webp";
+import img5 from "../images/494412358_18015234908706601_7858305182552597741_n.webp"
+import {Link} from "react-router-dom"
+import myImage from '../images/logo.png';
+
+const Gallery = () => {
+
+  return (
+    <div className="gallery">
+        <div className="logo-box">
+            <div className='logo-container'>
+                <img className="logo" alt="logo" src={myImage}></img>
+                <h2>Busy-Bee-Tree</h2>
+            </div>
+            <div className="logo-btn">
+                <Link to="/">
+                    <button className="back-btn">← Back to Home</button>
+                </Link>
+            </div>
+        </div>
+        <div className="media-container">
+            <div className="media">
+                <video controls>
+                    <source src={video1} type="video/mp4"/>
+                </video>
+                <p>"Where good vibes brew and memories stir."</p>
+            </div>
+            <div className="media">
+                <p>"You can’t make everyone happy... unless you’re pizza."</p>
+                <img src={img1}/>
+            </div>
+            <div className="media">
+                <img src={img2}/>
+                <p>"Stressed spelled backward is desserts… coincidence? We think not!"</p>
+            </div>
+            <div className="media">
+                <p>"Unwind where the city fades and the garden begins."</p>
+                <video controls>
+                    <source src={video2} type="video/mp4"/>
+                </video>
+            </div>
+            <div className="media">
+                <img src={img3}/>
+                <p>"Every pizza is a personal pizza if you believe in yourself."</p>
+            </div>
+            <div className="media">
+                <p>"Gather at the tree, where good moments grow."</p>
+                <video controls>
+                    <source src={video3} type="video/mp4"/>
+                </video>
+            </div>
+            <div className="media">
+                <img src={img4}/>
+                <p>"Because every story starts over a bowl of Maggi."</p>
+            </div>
+            <div className="media">
+                <p>"Where strangers become friends and friends become family."</p>
+                <img src={img5}/>
+            </div>
+            <div className="media">
+                <video controls>
+                    <source src={video4} type="video/mp4"/>
+                </video>
+                <p>"Nature’s hangout, brewed to perfection."</p>
+            </div>
+            <div className="media">
+                <p>"Rain in the garden, warmth on your plate."</p>           
+                <video controls>
+                    <source src={video5} type="video/mp4"/>
+                </video>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Gallery;
