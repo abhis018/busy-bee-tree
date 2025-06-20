78c943dfe83f5e5f1d2bcd2f2b0535b149ffa8ec
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-20 23:12:31 +0530

    added back button to the logo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,18 @@
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import myImage from '../images/logo.png';
 
 const Contact = () => {
 
   return (
-    <div className="gallery">
-        <Link to="/">
-            <button>Back to Home</button>
-        </Link>
+    <div className="contact">
+      <div className="logo-box">
+        <div className='logo-container'>
+          <Link to="/">
+            <img className="logo" alt="logo" src={myImage}></img>
+          </Link>
+          <h2>Busy-Bee-Tree</h2>
+        </div>
+      </div>
     </div>
   )
 };
