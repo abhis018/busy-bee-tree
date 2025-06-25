4ef1714d52d50d8d9ceb5301d77dfbc6c997baf3
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-06-25 23:11:58 +0530

    removed all the para from the section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,48 +18,38 @@ const Gallery = () => {
                 <video controls>
                     <source src={video1} type="video/mp4"/>
                 </video>
-                <p>"Where good vibes brew and memories stir."</p>
             </div>
             <div className="media">
-                <p>"You can’t make everyone happy... unless you’re pizza."</p>
                 <img alt="img" src={img1}/>
             </div>
             <div className="media">
                 <img alt="img" src={img2}/>
-                <p>"Stressed spelled backward is desserts… coincidence? We think not!"</p>
             </div>
             <div className="media">
-                <p>"Unwind where the city fades and the garden begins."</p>
                 <video controls>
                     <source src={video2} type="video/mp4"/>
                 </video>
             </div>
             <div className="media">
                 <img alt="img" src={img3}/>
-                <p>"Every pizza is a personal pizza if you believe in yourself."</p>
             </div>
             <div className="media">
-                <p>"Gather at the tree, where good moments grow."</p>
                 <video controls>
                     <source src={video3} type="video/mp4"/>
                 </video>
             </div>
             <div className="media">
                 <img alt="img" src={img4}/>
-                <p>"Because every story starts over a bowl of Maggi."</p>
             </div>
             <div className="media">
-                <p>"Where strangers become friends and friends become family."</p>
                 <img alt="img" src={img5}/>
             </div>
             <div className="media">
                 <video controls>
                     <source src={video4} type="video/mp4"/>
                 </video>
-                <p>"Nature’s hangout, brewed to perfection."</p>
             </div>
-            <div className="media">
-                <p>"Rain in the garden, warmth on your plate."</p>           
+            <div className="media">          
                 <video controls>
                     <source src={video5} type="video/mp4"/>
                 </video>
